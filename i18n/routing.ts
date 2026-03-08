e9dd500c5cf137e24476096dccd7567b9d2803ce
hcuervo719-prospectus <hcuervo719@gmail.com>
2026-03-07 19:09:06 -0500

    Add files via upload

---
+++ i18n/routing.ts
@@ -0,0 +1,10 @@
+import { defineRouting } from 'next-intl/routing';
+import { createSharedPathnamesNavigation } from 'next-intl/navigation';
+
+export const routing = defineRouting({
+  locales: ['es','en','pt','fr','de','it','nl','pl','tr','ja','ko','ru','hi','id','sv','no'],
+  defaultLocale: 'en',
+});
+
+export const { Link, redirect, usePathname, useRouter } =
+  createSharedPathnamesNavigation(routing);
