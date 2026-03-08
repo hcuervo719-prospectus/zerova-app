e0e7eac476f2bcc338d12db3b1ca8fcd2cb7c319
hcuervo719-prospectus <hcuervo719@gmail.com>
2026-03-07 19:53:19 -0500

    Add files via upload

--- request.ts
+++ request.ts
@@ -1,13 +1,15 @@
 import { getRequestConfig } from 'next-intl/server';
-import { routing } from './i18n/routing';
+import { locales, defaultLocale } from './i18n';
 
 export default getRequestConfig(async ({ requestLocale }) => {
   let locale = await requestLocale;
-  if (!locale || !routing.locales.includes(locale as any)) {
-    locale = routing.defaultLocale;
+
+  if (!locale || !locales.includes(locale as any)) {
+    locale = defaultLocale;
   }
+
   return {
     locale,
-    messages: (await import(`./messages/${locale}.json`)).default,
+    messages: (await import(`./messages/${locale}.json`)).default
   };
 });
