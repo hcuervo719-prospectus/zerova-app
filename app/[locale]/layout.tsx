6201eca05af08f1256cb59a79d4d2a2d0a184a88
hcuervo719-prospectus <hcuervo719@gmail.com>
2026-03-10 16:45:41 -0500

    Add files via upload

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,5 +1,5 @@
 import { NextIntlClientProvider } from 'next-intl'
-import { getMessages } from 'next-intl/server'
+import { getMessages, setRequestLocale } from 'next-intl/server'
 import { notFound } from 'next/navigation'
 import { routing } from '@/i18n/routing'
 
@@ -16,10 +16,13 @@ export default async function LocaleLayout({
     notFound()
   }
 
-  const messages = await getMessages()
+  // Tell next-intl which locale to use for this request
+  setRequestLocale(locale)
+
+  const messages = await getMessages({ locale })
 
   return (
-    <NextIntlClientProvider messages={messages}>
+    <NextIntlClientProvider locale={locale} messages={messages}>
       {children}
     </NextIntlClientProvider>
   )
